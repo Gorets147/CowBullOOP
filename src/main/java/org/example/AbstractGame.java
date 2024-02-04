21b82a48b6cef0948b5ac274d74acf9d743e807a
Gorets147 <mih.kom1998@gmail.com>
2024-02-04 20:19:09 +0300

    feat:initial commit

---
+++ src/main/java/org/example/AbstractGame.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public abstract class AbstractGame implements Game{
+    public String generateWord(){
+        return "";
+   }
+
+   public abstract void generateCharList();
+}
